cf2a8a329dc0c34563b5f6edd92c6164e846a88a
Jonathan Garza <jonathan.garza@rigup.com>
2019-12-17 13:24:18 -0600

    Comment out

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,10 +122,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # https://devcenter.heroku.com/articles/django-assets
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
